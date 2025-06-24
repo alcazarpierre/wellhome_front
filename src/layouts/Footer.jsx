2cbfe9c490dab15a172080db4d8e88f0028e01cd
alcazarpierre <alcazarpierre@gmail.com>
2025-06-24 09:35:18 -0500

    Contact phone updated

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -54,7 +54,7 @@ const Footer = () => {
                 </a>
                 <a href="https://wa.me/51987654321" target="_blank" rel="noopener noreferrer" className="flex items-center justify-center md:justify-start gap-2 hover:text-white transition-colors">
                     <WhatsappIcon className="w-5 h-5"/>
-                    <span>+51 987 654 321</span>
+                    <span>+51 924 707 719</span>
                 </a>
             </div>
           </div>
