ae58125326eb7cbe2d77afe1189449161d7710ce
alcazarpierre <alcazarpierre@gmail.com>
2025-06-25 16:10:28 -0500

    preregister form done

--- src/utils/validationSchemas.js
+++ src/utils/validationSchemas.js
@@ -22,9 +22,23 @@ export const contactSchema = z.object({
   }),
 });
 
-
 export const loginSchema = z.object({
   email: z.string().email({ message: "Por favor, introduce un correo electrónico válido." }),
   password: z.string().min(1, { message: "La contraseña no puede estar vacía." }),
   rememberMe: z.boolean().optional(),
+});
+
+export const preRegistrationSchema = z.object({
+  nombres: z.string().min(2, { message: "El nombre es requerido." }),
+  apellidos: z.string().min(2, { message: "El apellido es requerido." }),
+  email: z.string().email({ message: "Por favor, introduce un correo válido." }),
+  pais: z.string().min(1, { message: "Debes seleccionar un país." }),
+  estado: z.string().min(2, { message: "El estado/provincia es requerido." }),
+  ciudad: z.string().min(2, { message: "La ciudad es requerida." }),
+  distrito: z.string().min(2, { message: "El distrito es requerido." }),
+  celular: z.string().min(9, { message: "El número de celular no es válido." }),
+  nombreCondominio: z.string().min(3, { message: "El nombre del condominio es requerido." }),
+  aceptaTerminos: z.boolean().refine(data => data === true, {
+    message: "Debes aceptar los términos para registrarte.",
+  }),
 });
